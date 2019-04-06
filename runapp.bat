257acb4cbe709966891766bd971e8b2bfb836af6
Ajayvarma8142 <ajay.renati@gmail.com>
2019-04-06 11:02:46 +0000

    added

---
+++ runapp.bat
@@ -0,0 +1,4 @@
+del C:\progra~1\Apache Software Foundation\Tomcat 8.5\webapps\ROOT.war
+copy c:\progra~2\Jenkins\jobs\testing\branches\master\workspace\target\ROOT.war C:\progra~1\Apache Software Foundation\Tomcat 8.5\webapps
+
+
